7c4a376ea50d747deda40abaf43c99981cddde3c
flex2020 <61104736+flex2020@users.noreply.github.com>
2022-09-01 23:58:36 +0900

    실버1 1931 회의실 배정

---
+++ src/silver/p1931.java
@@ -0,0 +1,44 @@
+package silver;
+
+import javax.sound.midi.Soundbank;
+import java.util.*;
+
+public class p1931 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = sc.nextInt();
+        long[][] times = new long[N][2];
+        boolean[] check = new boolean[N];
+        for(int i=0; i<N; i++) {
+            times[i][0] = sc.nextLong();
+            times[i][1] = sc.nextLong();
+        }
+        long lastEnd = 0;
+        // sort
+        Arrays.sort(times, new Comparator<long[]>() {
+            @Override
+            public int compare(long[] o1, long[] o2) {
+                if(o1[1] > o2[1]) {
+                    if(o1[0] > o2[0]) return 1;
+                    else return 0;
+                }
+                else {
+                    if(o1[0] > o2[0]) return 1;
+                    else return 0;
+                }
+            }
+        });
+        for(int i=0; i<N; i++)
+        long cnt = 1;
+        long end = times[0][1];
+        for(int i=1; i<N; i++) {
+            if(end <= times[i][0]) {
+                cnt++;
+                end = times[i][1];
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
